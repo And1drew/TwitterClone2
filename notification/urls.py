8d88b04ceff93f9dd6a05a523ef2082ef42f2262
Andrew Belanger <andrew.j.belanger@gmail.com>
2020-09-03 12:02:48 -0400

    deepcopy created for refreshing notifications page and clearing new alerts

--- notification/urls.py
+++ notification/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from notification import views
 
 urlpatterns = [
-    path('seenotifications/', views.mark_as_seen),
     path('notifications/', views.notification_view),
 ]
--- notification/views.py
+++ notification/views.py
@@ -2,16 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from notification.models import Notification
+import copy
 # Create your views here.
 
 
 @login_required
 def notification_view(request):
     notifications = Notification.objects.filter(alert_for=request.user.id)
-    return render(request, 'notifications.html',{'notifications': notifications})
-
-
-@login_required
-def mark_as_seen(request):
+    notifications_copy = copy.copy(notifications)
     Notification.objects.filter(alert_for=request.user.id).delete()
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER','/'))
+    return render(request, 'notifications.html',{'notifications': notifications_copy})
