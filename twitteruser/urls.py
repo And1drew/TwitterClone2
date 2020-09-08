0aff810351b07919ef9d44e58040cca936043d7f
Andrew Belanger <andrew.j.belanger@gmail.com>
2020-09-08 11:21:40 -0400

    converted twitteruser.views to class based

--- twitteruser/urls.py
+++ twitteruser/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from twitteruser import views
 
 urlpatterns = [
-    path('', views.index),
-    path('signup/', views.signup_view),
-    path('profile/', views.profile_view),
-    path('user/<int:user_id>/', views.user_details),
-    path('follow/<int:user_id>/', views.follow_view),
-    path('unfollow/<int:user_id>/', views.unfollow_view),
+    path('', views.IndexView.as_view()),
+    path('signup/', views.SignUpView.as_view()),
+    path('profile/', views.ProfileView.as_view()),
+    path('user/<int:user_id>/', views.UserDetailsView.as_view()),
+    path('follow/<int:user_id>/', views.FollowView.as_view()),
+    path('unfollow/<int:user_id>/', views.UnfollowView.as_view()),
 ]
--- twitteruser/views.py
+++ twitteruser/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from twitteruser.models import TwitterUser
 from tweet.models import Tweet
 from notification.models import Notification
 from twitteruser.forms import signup_form
 
-# Create your views here.
-@login_required
-def index(request):
-    tweets = Tweet.objects.order_by('-date')
-    return render(request, 'index.html',{'tweets':tweets})
+
+class IndexView(LoginRequiredMixin, TemplateView):
+
+    def get(self, request):
+        tweets = Tweet.objects.order_by('-date')
+        return render(request, 'index.html',{'tweets':tweets})
 
 
-def signup_view(request):
-    if request.method == 'POST':
+class SignUpView(TemplateView):
+
+    def post(self, request):
         form = signup_form(request.POST)
         if form.is_valid():
             form = form.cleaned_data
@@ -23,39 +27,45 @@ def signup_view(request):
                 password=form.get('password'), 
                 displayname=form.get('displayname'), 
                 email=form.get('email'))
-            return HttpResponseRedirect('/')
-    form = signup_form()
-    return render(request, 'signup_form.html', {'form': form})
+            return HttpResponseRedirect('/') 
+
+    def get(self, request):
+        form = signup_form()
+        return render(request, 'signup_form.html', {'form': form})
 
 
+class UserDetailsView(TemplateView):
 
-def user_details(request, user_id):
-    user = TwitterUser.objects.get(id=user_id)
-    users_tweets = Tweet.objects.all()
-    followers = TwitterUser.objects.filter(follower=user).values('follower').count()
-    return render(request, 'user_details.html', {'user':user, 'tweets':users_tweets, 'followers':followers})
-
-
-@login_required
-def profile_view(request):
-    tweets = Tweet.objects.all()
-    my_tweets = Tweet.objects.filter(author=request.user).count()
-    followers = TwitterUser.objects.filter(follower=request.user).values('follower').count()
-    notifications = Notification.objects.filter(alert_for=request.user)
-    return render(request, 'profile.html', {'tweets':tweets, 'followers':followers, 'notifications':notifications, 'mytweets':my_tweets})
+    def get(self, request, user_id):
+        user = TwitterUser.objects.get(id=user_id)
+        users_tweets = Tweet.objects.all().order_by('-date')
+        followers = TwitterUser.objects.filter(follower=user).values('follower').count()
+        return render(request, 'user_details.html', {'user':user, 'tweets':users_tweets, 'followers':followers})
 
 
-@login_required
-def follow_view(request, user_id):
-    user_to_follow = TwitterUser.objects.filter(id=user_id).first()
-    request.user.follower.add(user_to_follow)
-    request.user.save()
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER','/'))
+class ProfileView(LoginRequiredMixin, TemplateView):
+
+    def get(self, request):
+        tweets = Tweet.objects.all().order_by('-date')
+        my_tweets = Tweet.objects.filter(author=request.user).count()
+        followers = TwitterUser.objects.filter(follower=request.user).values('follower').count()
+        notifications = Notification.objects.filter(alert_for=request.user)
+        return render(request, 'profile.html', {'tweets':tweets, 'followers':followers, 'notifications':notifications, 'mytweets':my_tweets})
 
 
-@login_required
-def unfollow_view(request, user_id):
-    user_to_unfollow = TwitterUser.objects.filter(id=user_id).first()
-    request.user.follower.remove(user_to_unfollow)
-    request.user.save()
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER','/'))
+class FollowView(LoginRequiredMixin, TemplateView):
+
+    def get(self, request, user_id):
+        user_to_follow = TwitterUser.objects.filter(id=user_id).first()
+        request.user.follower.add(user_to_follow)
+        request.user.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER','/'))
+
+
+class UnfollowView(LoginRequiredMixin, TemplateView):
+    
+    def get(self, request, user_id):
+        user_to_unfollow = TwitterUser.objects.filter(id=user_id).first()
+        request.user.follower.remove(user_to_unfollow)
+        request.user.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER','/'))
